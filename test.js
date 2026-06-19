b432d92931bd2f27833fad43c9fd8233692dc04e
khalidbasapur <khalid.basapur@gmail.com>
2026-06-19 20:20:28 +0530

    Initial commit for self healing CI/CD pipeline

---
+++ test.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+
+  http.get('http://localhost:3000', (res) => {
+    console.log(`Status Code: ${res.statusCode}`);
+
+    if(res.statusCode === 200) {
+      console.log('Test passed: Received 200 OK');
+      server.close();
+      process.exit(0);
+    } else {
+        server.close();
+        process.exit(1);
+    }
+  }).on('error', (err) => {
+    console.error(`Test failed: ${err.message}`);
+    server.close();
+    process.exit(1);
+  });
+});
